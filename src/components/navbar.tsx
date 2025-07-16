21b6f81a5d2995807a6ff57c890df22f1014801b
mohammadreza2003-af <mohammadreza2003.af@gmail.com>
2025-07-16 16:14:38 +0430

    refactor(ui) add title in navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,10 @@ const Navbar = () => {
   return (
     <nav className="md:max-w-3xl px-4 md:px-0 py-8 w-full mx-auto">
       <div className="flex items-center justify-between">
-        <Book size={32} />
+        <div className="flex items-center gap-2">
+          <Book size={32} />
+          <h2 className="font-semibold text-xl">Dictionary</h2>
+        </div>
         <div className="flex items-center gap-4">
           <FontSelector />
           <div className="w-[0.5px] min-h-8 bg-foreground" />
